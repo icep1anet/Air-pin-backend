72a93af9b1ec237170e61d67ecd3b6b1a54fb2fd
Nakao-Isshin <katyou7777@gmail.com>
2023-12-23 17:36:16 +0900

    ビルの1階あたりの高さを計算する関数を追加

--- src/api/height/root.py
+++ src/api/height/root.py
@@ -4,8 +4,11 @@ height_router = APIRouter()
 
 
 @height_router.get("/")
-async def root():
-    return {"message": "Hello Height"}
+async def height(destName:str, destAddress:str, destLat:float, destLong:float):
+    ceiling_height = get_building_height(destLat, destLong)
+    if ceiling_height is None:
+        ceiling_height = 3.0
+    return {"target_height": ceiling_height}
 
 @height_router.get("/test")
 async def test():
